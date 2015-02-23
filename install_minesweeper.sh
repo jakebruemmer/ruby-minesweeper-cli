05f884e819eafd0dc62a9b205121fefa30930a73
gr1zzly-be4r <jake.bruemmer@gmail.com>
2015-02-23 10:07:12 -0600

    Added gemspec, made a gem, and removed shell script for installation.

--- install_minesweeper.sh
+++
@@ -1,9 +0,0 @@
-mkdir $HOME/.minesweeper
-mv minesweeper* game* $HOME/.minesweeper/
-cd $HOME/.minesweeper/
-
-chmod +x ./minesweeper.rb
-
-mv minesweeper.rb minesweeper
-
-sudo ln -s $PWD/minesweeper /usr/local/bin
