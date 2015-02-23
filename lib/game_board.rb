09e7a081550ceff29f4498368a56a8d5c6d35d92
gr1zzly-be4r <jake.bruemmer@gmail.com>
2015-02-23 10:44:11 -0600

    Changed require_relative to require in order to make the gem executable work

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -1,6 +1,6 @@
 # GameBoard class will be used by the minesweeper game to keep track of tiles, 
 # bombs, and the conditions necessary to win the game.
-require_relative 'game_tile'
+require 'game_tile'
 
 class GameBoard
 
